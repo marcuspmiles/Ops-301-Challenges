5a15654d46668870dc03d08beff9a4ff220fe72d
Marcus Miles <marcuspmiles@gmail.com>
2021-09-07 16:27:58 -0700

    Update O301CH6.py

--- O301CH6.py
+++ O301CH6.py
@@ -5,17 +5,22 @@
 
 print ("Welcome to my script")
 
+# Importing the OS
+import os
+
+# Getting user ID
+print ("%%%% WHO AM I? %%%%")
+euid = os.geteuid()
+print("User ID is:", euid)
+
+# Getting Hardware Information
+print ("%%%% FETCH HARDWARE INFO %%%%")
+hardware = os.uname()
+print("Hardware:", hardware)
+
+# Getting Username
 print ("%%%%%% WHOAMI %%%%%%")
 import getpass
 print(getpass.getuser())
 
-print ("%%%%%% LIST HARDWARE %%%%%%")
-print ("%%% PLATFORM %%%")
-import platform
-print(platform.system())
-print ("%%% RELEASE %%%")
-print(platform.release())
-print ("%%% VERSION %%%")
-print(platform.version())
-print ("%%% PROCESSOR %%%")
-print(platform.processor())
+
