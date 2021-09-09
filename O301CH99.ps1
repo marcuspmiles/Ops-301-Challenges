4954afe20aeeb9ac8c8aa4ad1423f8c41ff08b74
Marcus Miles <marcuspmiles@gmail.com>
2021-09-09 15:00:02 -0700

    Update and rename O301CH8.ps1 to O301CH99.ps1

--- O301CH99.ps1
+++ O301CH99.ps1
@@ -1,3 +1,5 @@
 Import-Module ActiveDirectory
+# Importing the AD
 
+# Adding a user with all their information
 New-ADUser -Name "Franz Ferdinand" -GivenName "Franz" -Surname "Ferdinand" -SamAccountName "F.Ferdinand" -UserPrincipalName "ferdi@GlobeXpower.com" -Title "TPS Reporting Lead" -Company "GlobeX USA" -Location "Springfield, OR"
