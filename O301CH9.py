dace03233deca5059fdc7ca6356f03dd831b1648
Marcus Miles <marcuspmiles@gmail.com>
2021-09-10 14:17:14 -0700

    Create O301CH9.py

---
+++ O301CH9.py
@@ -0,0 +1,23 @@
+# Python Script
+
+# Defining a list
+list = ['Cookie','John','Tree','List','Paper','Test','Cake','Igloo','Bar','Restaurant']
+print("----Entire List----")
+print(list)
+
+# Adding a variable to every item in the list
+v1, v2, v3, v4, v5, v6, v7, v8, v9, v10 = ['Cookie','John','Tree','List','Paper','Test','Cake','Igloo','Bar','Re>
+# Printing value 4 / variable 4
+print("----Fourth Element----")
+print(v4)
+
+# Printing 6-10th value
+print("----Sixth --> Tenth Element----")        
+print(v6,v7,v8,v9,v10)
+
+print("----Changing Seventh Value to ONION----")
+print("VALUE 7 BEFORE")
+print(v7)
+print("VALUE 7 AFTER")
+# Replacing 7th value to something else
+print(v7.replace("Cake", "ONION",))
