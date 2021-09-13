91ce37b6e5048e24d8a84a8fe2759cdb97869e84
Marcus Miles <marcuspmiles@gmail.com>
2021-09-13 16:31:29 -0700

    Create O301CH10.py

---
+++ O301CH10.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Script to check if 100 is <, ==, >, !=, to a certain value
+
+var = 100
+if var == 105:
+   print ("100 is equal to 100")
+elif var != 100:
+   print ("100 is not equal to 105")
+if var != 101:
+   print ("100 is not equal to 101")
+else:
+   print ("100 is equal to 101")
+if var < 105:
+   print ("100 is less than 105")
+else:
+   print ("100 is not less than 105")
+if var <= 105:
+   print ("100 is less than or equal to 105")
+if var > 99:
+   print ("100 is greater than 99")
+if var >= 100:
+   print ("100 is greater than or equal to 100")
